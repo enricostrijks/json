d4cc01a576ff897f2d924ff2e8123fba99e21024
Enrico <strijksenrico@gmail.com>
2020-11-03 11:49:19 +0100

    Data uit JSON stap: 3

--- js/boeken.js
+++ js/boeken.js
@@ -4,13 +4,20 @@ const xhr = new XMLHttpRequest();
 xhr.onreadystatechange = () => {
   if (xhr.readyState == 4 && xhr.status == 200) {
     let result = JSON.parse(xhr.responseText);
-    console.log(result);
-  } else {
-    console.log("readystate: " + xhr.readyState);
-    console.log("status: " + xhr.status);
+    boeken.data = result
+    boeken.uitvoeren();
   }
-};
+}
 xhr.open("GET", "boeken.json", true);
 xhr.send();
 
-console.log("Dit is de laatste regel script nu");
+const boeken = {
+
+  uitvoeren() {
+    let html = "";
+    this.data.forEach( boek => {
+      html += `<h3>${boek.titel}</h3>`
+    });
+    uitvoer.innerHTML = html
+  }
+}
