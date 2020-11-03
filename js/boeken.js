17fd2cbad90db0e8a45e39b155468e48c4b5c849
Enrico <strijksenrico@gmail.com>
2020-11-03 13:23:44 +0100

    Data JSON stap 11

--- js/boeken.js
+++ js/boeken.js
@@ -39,7 +39,7 @@ const boeken = {
       html += `<img class="boek__cover" src="${boek.cover}" alt ="${titel}">`;
       html += `<h3 class="boek_kop">${titel}</h3>`;
       html += `<p class="boek_auteurs">${auteurs}</p>`;
-      html += `<span class="boek_uitgave">${boek.uitgave}</span>`;
+      html += `<span class="boek_uitgave">${this.datumOmzetten(boek.uitgave)}</span>`;
       html += `<span class="boek_ean">EAN: ${boek.ean}</span>`;
       html += `<span class="boek_paginas">${boek.paginas} pagina's</span>`;
       html += `<span class="boek_taal">${boek.taal}</span>`;
@@ -47,5 +47,30 @@ const boeken = {
       html += `</section>`
     });
     uitvoer.innerHTML = html
+  },
+  datumOmzetten(datumString) {
+    let datum = new Date(datumString);
+    let jaar = datum.getFullYear();
+    let maand = this.geefMaandnaam(datum.getMonth());
+    return `${maand} ${jaar}`;
+  },
+  geefMaandnaam(m) {
+    let maand = "";
+    switch(m) {
+      case 0 : maand = 'januari'; break;
+      case 1 : maand = 'februari'; break;
+      case 2 : maand = 'maart'; break;
+      case 3 : maand = 'april'; break;
+      case 4 : maand = 'mei'; break;
+      case 5 : maand = 'juni'; break;
+      case 6 : maand = 'juli'; break;
+      case 7 : maand = 'augustus'; break;
+      case 8 : maand = 'september'; break;
+      case 9 : maand = 'oktober'; break;
+      case 10 : maand = 'november'; break;
+      case 11 : maand = 'december'; break;
+      default : maand = m;
+    }
+    return maand;
   }
 }
